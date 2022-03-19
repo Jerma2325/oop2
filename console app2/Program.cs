35c0fbd0f74f7395c85a5bf23d29201a438ae713
Tymofii Kryvosheia <tymofii.kryvosheia@microsoft.wsei.edu.pl>
2022-03-19 19:29:33 +0100

    Add project files.

---
+++ console app2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Teacher treacher = new Teacher("Maria Skłodowska", 50);
+
+            Student student1 = new Student("Jan Kowaslski", 21, "LAB-01");
+            Student student2 = new Student("Jan Kowaslski", 21, "LAB-01");
+            Student student3 = new Student("Jaś Fasola", 23, "LAB-02");
+
+            student1.AddTask("Taks A", TaskStatus.Waiting);
+            student1.AddTask("Taks B", TaskStatus.Waiting);
+            student1.AddTask("Taks C", TaskStatus.Rejected);
+
+            student2.AddTask("Taks A", TaskStatus.Waiting);
+            student2.AddTask("Taks B", TaskStatus.Waiting);
+            student2.AddTask("Taks C", TaskStatus.Rejected);
+
+            student3.AddTask("Taks D", TaskStatus.Done);
+            student3.AddTask("Taks E", TaskStatus.Waiting);
+            student3.AddTask("Taks F", TaskStatus.Waiting);
+
+            student3.UpdateTask(1, TaskStatus.Done);
+            student3.UpdateTask(2, TaskStatus.Progress);
+
+            Person[] persons = { treacher, student1, student2, student3 };
+            Classroom classroom = new Classroom("Sala Komputerowa", persons);
+
+            Console.WriteLine(classroom);
+
+            Console.WriteLine("student1 == student2: " + student1.Equals(student2)); // Output: student1 == student2: true
+            Console.WriteLine("student1 == student3: " + student1.Equals(student3)); // Output: student1 == student3: false
+        }
+    }
+
+    public class Person
+    {
+        protected string name;
+        protected int age;
+
+         public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Person p = (Person)obj;
+                return name.Equals(p.name) && age.Equals(p.age);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(name, age).GetHashCode();
+        }
+        public override string ToString()
+        {
+            return name.ToString() + ("(")+ age.ToString() + ("y.o.)");
+        }
+
+
+    }
+
+    public class Student
+    {
+        protected string group;
+         
+    }
+
+    public class Task
+    {
+        private Person person;
+        public string Name
+        {
+            get { return person.Name; }
+             
+        }
+        public TaskStatus;
+    }
+    public class TaskStatus
+    {
+
+    }
+
+    
+}
